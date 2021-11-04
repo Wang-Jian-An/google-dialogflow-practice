72c79610eb9760945701bc313b53a2771d175000
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2021-11-04 15:57:29 +0800

    Update create_agents.py

--- create_agents.py
+++ create_agents.py
@@ -3,27 +3,28 @@ import uuid
 import pytest
 from google.cloud.dialogflowcx_v3.services.agents.client import AgentsClient
 from google.cloud.dialogflowcx_v3.types.agent import Agent
+from google.cloud.dialogflowcx_v3.types import CreateAgentRequest
 
 # PROJECT_ID = os.getenv("create-agent-with-api-6343d801abf5.json")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "create-agent-with-api-6343d801abf5.json"
 pytest.AGENT_PATH = ""
-
-def create_agent(project_id, display_name):
-
-    parent = "projects/" + project_id + "/locations/global"
-
-    agents_client = AgentsClient()
-
-    agent = Agent(
-        display_name=display_name,
-        default_language_code="en",
-        time_zone="America/Los_Angeles",
-    )
-
-    response = agents_client.create_agent(request={"agent": agent, "parent": parent})
-
-    return response
-
 agentName = f"fake_agent_{uuid.uuid4()}"
-response = create_agent("create-agent-with-api", agentName)
+
+# 建立服務機制
+agents_client = AgentsClient()
+
+
+# 設定服務基本架構
+agent = Agent(
+    display_name=agentName,
+    default_language_code="en",
+    time_zone="America/Los_Angeles"
+)
+
+# 設定服務需求
+project_id = "create-agent-with-api"
+parent = f"projects/{project_id}/locations/global"
+agent_request = CreateAgentRequest(parent=parent, agent=agent)
+
+response = agents_client.create_agent(agent_request)
 print(response)
